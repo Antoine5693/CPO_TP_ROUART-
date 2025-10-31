3b5a15c3a01b7c1c14e40564b3fdd936443b1b57
Antoine Rouart <antoine.rouart@epfedu.fr>
2025-10-31 18:38:56 +0100

    Création d'un nouveau projet versionné

---
+++ TP1_convertisseur_ROUART/src/Scanner.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Rouart Antoine
+ */
+class Scanner {
+
+    double nextDouble() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void close() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
