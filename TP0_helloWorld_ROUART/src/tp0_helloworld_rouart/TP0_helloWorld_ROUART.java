d5e107bc7ce555458c19c8f59d6468eb182c3ffe
Antoine Rouart <antoine.rouart@epfedu.fr>
2025-10-31 18:40:16 +0100

    Création d'un nouveau projet versionné

---
+++ TP0_helloWorld_ROUART/src/tp0_helloworld_rouart/TP0_helloWorld_ROUART.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloworld_rouart;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rouart Antoine
+ */
+public class TP0_helloWorld_ROUART {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println( " Bonjour " ) ; 
+        Scanner sc ;
+        sc = new Scanner (System.in) ;
+        System.out.println("Quel est votre prénom ?");
+        sc.nextLine();  
+    }
+    
+}
