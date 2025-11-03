d150408eafdf59577c1045eedc40338bfbd2ba19
Antoine Rouart <antoine.rouart@epfedu.fr>
2025-11-03 18:57:43 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_Bieres_ROUART/src/tp2_bieres_rouart/TP2_Bieres_ROUART.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_rouart;
+
+/**
+ *
+ * @author Rouart Antoine
+ */
+public class TP2_Bieres_ROUART {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson");        
+        uneBiere.lireEtiquette();
+        BouteilleBiere deuxbiere = new BouteilleBiere("Leffe",6.6 ,"Abbaye de Leffe");
+        deuxbiere.lireEtiquette();
+              
+    }
+    
+}
