cf80e98a8689f92712c87caa31f50eaf2cf3e6c4
Antoine Rouart <antoine.rouart@epfedu.fr>
2025-11-04 12:59:51 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_convertisseurObjet_ROUART/src/tp2_convertisseurobjet_rouart/Convertisseur.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_rouart;
+
+/**
+ *
+ * @author Rouart Antoine
+ */
+public class Convertisseur {
+    int nbConversions;
+    
+    public Convertisseur(){
+        nbConversions = 0 ; 
+        System.out.println("Convertisseur éxécuté !");
+        
+    }
+       // Celius à Kelvin
+    public double CelsiusVersKelvin(double valeurCelsius) {
+        nbConversions++;
+        return valeurCelsius + 273.15;
+    }
+
+    // Kelvin à Celsius
+    public double KelvinVersCelsius(double valeurKelvin) {
+        nbConversions++;
+        return valeurKelvin - 273.15;
+        
+    }
+
+    // Fahrenheit à Celsius
+    public double FahrenheitVersCelsius(double valeurFahrenheit) {
+        nbConversions++;
+        return (valeurFahrenheit - 32) * 5 / 9;
+    }
+
+    // Celsius à Fahrenheit
+    public double CelsiusVersFahrenheit(double valeurCelsius) {
+        nbConversions++;
+        return (valeurCelsius * 9 / 5) + 32;
+    }
+
+    // Kelvin à Fahrenheit
+    public double KelvinVersFahrenheit(double valeurKelvin) {
+        nbConversions++;
+        double valeurCelsius = KelvinVersCelsius(valeurKelvin);
+        return CelsiusVersFahrenheit(valeurCelsius);
+    }
+
+    // Fahrenheit à Kelvin
+    public double FahrenheitVersKelvin(double valeurFahrenheit) {
+        nbConversions++;
+        double valeurCelsius = FahrenheitVersCelsius(valeurFahrenheit);
+        return CelsiusVersKelvin(valeurCelsius);
+        
+        }
+    @Override 
+    public String toString () 
+    { return "nb de conversions"+ nbConversions; } 
+}
---
+++ TP2_convertisseurObjet_ROUART/src/tp2_convertisseurobjet_rouart/TP2_convertisseurObjet_ROUART.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_rouart;
+
+/**
+ *
+ * @author Rouart Antoine
+ */
+public class TP2_convertisseurObjet_ROUART {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Convertisseur A = new Convertisseur();
+        A.FahrenheitVersKelvin(86);
+        System.out.println(A);
+    }
+    
+}
